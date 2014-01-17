a9880738470d978bee2bc8bf3c60068758c4b570
StivenR <stiven.ramond@cifacom.com>
2014-01-17 15:30:42 +0100

    lists like/viewed/whish

--- api/controllers/DogsController.php
+++ api/controllers/DogsController.php
@@ -3,87 +3,236 @@ include('C:\wamp\www\f3-api\api\models\Data.php');
 class DogsController{
 
 	public function __construct(){
+		$data_model = new Data;
+		$key = (isset($_GET['token']))?$_GET['token']:'';
+			
+		$this->key = $data_model->keyExists($key);
+		if($this->key){
+			$this->admin = $data_model->isAdmin($key);
+		}else{
+			$this->admin = false;
+		}
 	}
 
 	public function actionFindUsers(){
-		$data_model = new Data;
-		$data['Users'] = $data_model->getUsers();
-		Api::response(200, $data);
+		if($this->admin){
+			$data_model = new Data;
+			$data['Users'] = $data_model->getUsers();
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Admin'));
+		}
 	}
 
 	public function actionCreateUser(){
-		if(isset($_POST['name']) && isset($_POST['firstname']) && isset($_POST['email']) && isset($_POST['pass'])){
-			$data = array('Create user with name ' . $_POST['name'] . ' and email ' . $_POST['email']);
-			$data_model = new Data;
-			$data['Status'] = $data_model->createUser($_POST['name'],$_POST['firstname'],$_POST['email'],md5($_POST['pass']));
-			Api::response(200, $data);
-		}
-		else{
-			Api::response(400, array('error'=>'Error'));
+		if($this->admin){
+			if(isset($_POST['name']) && isset($_POST['firstname']) && isset($_POST['email']) && isset($_POST['pass'])){
+				$data['Process'] = array('Create user with name ' . $_POST['name'] . ' and email ' . $_POST['email']);
+				$data_model = new Data;
+				$data['Status'] = $data_model->createUser($_POST['name'],$_POST['firstname'],$_POST['email'],md5($_POST['pass']));
+				Api::response(200, $data);
+			}
+			else{
+				Api::response(400, array('error'=>'Error'));
+			}
+		}else{
+			Api::response(403, array('error'=>'Admin'));
 		}
 	}
 
 	public function actionFindOneUser(){
-		$data_model = new Data;
-		$id = F3::get('PARAMS.id');
-		$data['User'] = $data_model->getUser($id);
-		Api::response(200, $data);
+		if($this->admin){
+			$data_model = new Data;
+			$id = F3::get('PARAMS.id');
+			$data['User'] = $data_model->getUser($id);
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Admin'));
+		}
 	}
 
 	public function actionUpdateUser(){
-		$id = F3::get('PARAMS.id');
-		$values = array('name'=>F3::get('PUT.name'),'firstname'=>F3::get('PUT.firstname'),'email'=>F3::get('PUT.email'),'pass'=>md5(F3::get('PUT.pass'));
-		$data_model = new Data;
-		$data['Movie'] = $data_model->getMovie($id,$values);
-		Api::response(200, $data);
+		if($this->admin){
+			$id = F3::get('PARAMS.id');
+			$put = PUT::get();
+			$name=(!isset($put['name']))?false:$put['name'];
+			$firstname=(!isset($put['firstname']))?false:$put['firstname'];
+			$email=(!isset($put['email']))?false:$put['email'];
+			$pass=(!isset($put['password']))?false:md5($put['password']);
+			$values = array('name'=>$name,'firstname'=>$firstname,'email'=>$email,'pass'=>$pass);
+			$param = false;
+			foreach($values as $val){
+				//Check if at least one param is set, else return an error
+				if($val){
+					$param = true;
+				}
+			}
+			if($param){
+				$data_model = new Data;
+				$data = $data_model->updateUser($id,$values);
+			}else{
+				$data = "Aucun paramètre modifié, veuillez vérifier vos options (name, firstname, email, password)";
+			}
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Admin'));
+		}
 	}
 
 	public function actionDeleteUser(){
-		$id = F3::get('PARAMS.id');
-		$data_model = new Data;
-		$data['User'] = $data_model->deleteUser($id);
-		$data['Status'] = 'Delete user with name: ' . $id;
-		Api::response(200, $data);
+		if($this->admin){
+			$id = F3::get('PARAMS.id');
+			$data_model = new Data;
+			$data['User'] = $data_model->deleteUser($id);
+			$data['Status'] = 'Delete user with name: ' . $id;
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Admin'));
+		}
 	}
 
 	public function actionFindMovies(){
-		$data_model = new Data;
-		$data['Movies'] = $data_model->getMovies();
-		Api::response(200, $data);
+		if($this->key){
+			$data_model = new Data;
+			$response = $data_model->getMovies();
+			if($response){
+				$data['Movies'] = $response;
+			}
+			else{
+				$data['Movies'] = "Aucun film";
+			}
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Key access'));
+		}
 	}
 
 	public function actionCreateMovie(){
-		if(isset($_POST['name'])){
-			$data = array('Create Movie with name ' . $_POST['name']);
-			$data_model = new Data;
-			$data['Status'] = $data_model->createMovie($_POST['name'],$_POST['release'],$_POST['synopsis']);
-			Api::response(200, $data);
-		}
-		else{
-			Api::response(400, array('error'=>'Name is missing'));
+		if($this->admin){
+			if(isset($_POST['name'])){
+				$data = array('Create Movie with name ' . $_POST['name']);
+				$data_model = new Data;
+				$data['Status'] = $data_model->createMovie($_POST['name'],$_POST['release'],$_POST['synopsis']);
+				Api::response(200, $data);
+			}
+			else{
+				Api::response(400, array('error'=>'Name is missing'));
+			}
+		}else{
+			Api::response(400, array('error'=>'Key access'));
 		}
 	}
 
 	public function actionFindOneMovie(){
-		$id = F3::get('PARAMS.id');
-		$data_model = new Data;
-		$data['Movie'] = $data_model->getMovie($id);
-		Api::response(200, $data);
+		if($this->key){
+			$id = F3::get('PARAMS.id');
+			$data_model = new Data;
+			$data['Movie'] = $data_model->getMovie($id);
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Key access'));
+		}
 	}
 
 	public function actionUpdateMovie(){
-		$id = F3::get('PARAMS.id');
-		$values = array('name'=>F3::get('PUT.name'),'release'=>F3::get('PUT.release'),'date'=>F3::get('PUT.date'),'synopsis'=>F3::get('PUT.synopsis'));
-		$data_model = new Data;
-		$data['Movie'] = $data_model->getMovie($id,$values);
-		Api::response(200, $data);
+		if($this->admin){
+			$id = F3::get('PARAMS.id');
+			$put = PUT::get();
+			$name=(!isset($put['name']))?false:$put['name'];
+			$release=(!isset($put['release']))?false:$put['release'];
+			$synopsis=(!isset($put['synopsis']))?false:$put['synopsis'];
+			$values = array('name'=>$name,'release'=>$release,'synopsis'=>$synopsis);
+			$param = false;
+			foreach($values as $val){
+				//Check if at least one param is set, else return an error
+				if($val){
+					$param = true;
+				}
+			}
+			if($param){
+				$data_model = new Data;
+				$data = $data_model->updateMovie($id,$values);
+			}else{
+				$data = "Aucun paramètre modifié, veuillez vérifier vos options (name, release, synopsis)";
+			}
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Key access'));
+		}
 	}
 
 	public function actionDeleteMovie(){
-		$id = F3::get('PARAMS.id');
-		$data_model = new Data;
-		$data['Movie'] = $data_model->deleteMovie($id);
-		$data['Status'] = 'Delete Movie with id: ' . $id;
-		Api::response(200, $data);
+		if($this->admin){
+			$id = F3::get('PARAMS.id');
+			$data_model = new Data;
+			$data['Movie'] = $data_model->deleteMovie($id);
+			$data['Status'] = 'Delete Movie with id: ' . $id;
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Key access'));
+		}
+	}
+
+	public function actionFindLikes(){
+		if($this->key){
+			$data_model = new Data;
+			$data['Likes'] = $data_model->getLikes($_GET['token']);
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Key access'));
+		}
+	}
+
+	public function actionFindWishes(){
+		if($this->key){
+			$data_model = new Data;
+			$data['Wishes'] = $data_model->getWishes($_GET['token']);
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Key access'));
+		}
+	}
+
+	public function actionFindViewed(){
+		if($this->key){
+			$data_model = new Data;
+			$data['Viewed'] = $data_model->getViewed($_GET['token']);
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Key access'));
+		}
+	}
+
+	public function actionFindLikesFor(){
+		if($this->admin){
+			$id = F3::get('PARAMS.id');
+			$data_model = new Data;
+			$data['Likes'] = $data_model->getLikesFor($id);
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Key access'));
+		}
+	}
+
+	public function actionFindWishesFor(){
+		if($this->admin){
+			$id = F3::get('PARAMS.id');
+			$data_model = new Data;
+			$data['Wishes'] = $data_model->getWishesFor($id);
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Key access'));
+		}
+	}
+
+	public function actionFindViewedFor(){
+		if($this->admin){
+			$id = F3::get('PARAMS.id');
+			$data_model = new Data;
+			$data['Viewed'] = $data_model->getViewedFor($id);
+			Api::response(200, $data);
+		}else{
+			Api::response(400, array('error'=>'Key access'));
+		}
 	}
 }
--- api/models/Data.php
+++ api/models/Data.php
@@ -7,29 +7,40 @@
 	}
 
 	/*
-	*Create User
+	*Create User, token is auto-generate and group is set to 0.
 	*/
 	public function createUser($name,$firstname,$email,$pass){
-		$sql = 'SELECT count(id) from users WHERE users.email="'.$email.'"';
-		if($this->db->exec($sql) == 0){
-			$status = $this->db->exec('INSERT INTO users VALUES("","'.$name.'","'.$firstname.'","'.$email.'","' . $pass . '")');
+		$sql = 'SELECT id from users WHERE users.email="'.$email.'"';
+		if(count($this->db->exec($sql)) == 0){
+			$req = $this->db->exec('INSERT INTO users VALUES("","'.$name.'","'.$firstname.'","'.$email.'","' . $pass . '","'.sha1(md5($email)).'","2")');
+			$status['Clé'] = 'Votre access token : '.sha1(md5($email));
 		}else{
-			$status = 'Email déjà existante';
+			$status['?'] = 'Email déjà existante';
+			
 		}
 		return $status;
 	}
 
 	/*
-	*Update User
+	*Update User By ID and values (array[name, firstname, email, pass, group]), Token can't be update
 	*/
 	public function updateUser($id,$values){
 		$user = $this->getUser($id);
-		$this->db->exec('UPDATE users WHERE VALUES("","'.$name.'","'.$firstname.'","'.$email.'","'.md5($pass).'")');
+		$name = (!$values['name'])?$user['Name']:$values['name'];
+		$firstname = (!$values['firstname'])?$user['Firstname']:$values['firstname'];
+		$email = (!$values['email'])?$user['Email']:$values['email'];
+		$pass = (!$values['pass'])?$user['Password']:$values['pass'];
+		$group = (!$values['group'])?$user['Group']:$values['group'];
+		$this->db->exec('UPDATE users SET name="'.$name.'",firstname="'.$firstname.'",email="'.$email.'",pwd="'.$pass.'",user_group="'.$group.'" WHERE users.id="' .$id . '"');
+		$new = $this->getUser($id);
+		$data['Before'] = $user;
+		$data['After'] = $new;
+		return $data;
 	}
 
 
 	/*
-	*Get User
+	*Get All Users
 	*/
 	public function getUsers(){
 		$users = $this->db->exec('SELECT id as Identifiant, name as Name, firstname as Firstname, email as Email, pwd as Password FROM users');
@@ -37,15 +48,15 @@
 	}
 
 	/*
-	*Get User
+	*Get One User By ID
 	*/
 	public function getUser($id){
-		$user = $this->db->exec('SELECT id as Identifiant, name as Name, firstname as Firstname, email as Email, pwd as Password FROM users WHERE users.id = "' . $id . '"');
+		$user = $this->db->query('SELECT id as Identifiant, name as Name, firstname as Firstname, email as Email, pwd as Password FROM users WHERE users.id = "' . $id . '"')->fetch(PDO::FETCH_ASSOC);
 		return $user;
 	}
 
 	/*
-	*Delete User
+	*Delete One User By ID
 	*/
 	public function deleteUser($id){
 		$item = $this->getUser($id);
@@ -54,11 +65,11 @@
 	}
 
 	/*
-	*Create Movie
+	*Create Movie (Name, Release Date, Synopsis)
 	*/
 	public function createMovie($name,$release_date,$synopsis){
-		$sql = 'SELECT count(id) from movies WHERE movies.name="'.$name.'"';
-		if($this->db->exec($sql) == 0){
+		$sql = 'SELECT id from movies WHERE movies.name="'.$name.'"';
+		if(count($this->db->exec($sql)) == 0){
 			$status = $this->db->exec('INSERT INTO movies VALUES("","' . $name . '","' . $release_date . '","' . $synopsis .'")');
 		}else{
 			$status = 'Film déjà existant';
@@ -67,14 +78,22 @@
 	}
 
 	/*
-	*Update Movie
+	*Update Movie by ID with values (array[name, release, synopsis])
 	*/
 	public function updateMovie($id, $values){
-		$movie = $this->$getMovie($id);
+		$movie = $this->getMovie($id);
+		$name = (!$values['name'])?$movie['Name']:$values['name'];
+		$release = (!$values['release'])?$movie['Release']:$values['release'];
+		$synopsis = (!$values['synopsis'])?$movie['Synopsis']:$values['synopsis'];
+		$this->db->exec('UPDATE users SET name="'.$name.'",release="'.$release.'",synopsis="'.$synopsis.'" WHERE movies.id="' .$id . '"');
+		$new = $this->getMovie($id);
+		$data['Before'] = $movie;
+		$data['After'] = $new;
+		return $data;
 	}
 
 	/*
-	*Get Movies
+	*Get All Movies
 	*/
 	public function getMovies(){
 		$movies = $this->db->exec('SELECT id as Identifiant, name as Name, release_date as Release_Date, synopsis as Synopsis FROM movies');
@@ -82,19 +101,84 @@
 	}
 
 	/*
-	*Get Movie
+	*Get One Movie By ID
 	*/
 	public function getMovie($id){
-		$movie = $this->db->exec('SELECT id as Identifiant, name as Name, release_date as Release_Date, synopsis as Synopsis FROM movies WHERE movies.id = "' . $id . '"');
+		$movie = $this->db->query('SELECT id as Identifiant, name as Name, release_date as Release_Date, synopsis as Synopsis FROM movies WHERE movies.id = "' . $id . '"')->fetch(PDO::FETCH_ASSOC);
 		return $movie;
 	}
 
 	/*
-	*Delete Movie
+	*Delete One Movie By ID
 	*/
 	public function deleteMovie($id){
 		$item = $this->getMovie($id);
 		$this->db->exec('DELETE FROM movies WHERE movies.id = "' . $id . '"');
 		return $item;
+	}
+
+	/*
+	*Chek if token key exists
+	*/
+	public function keyExists($key){
+		$response = $this->db->exec('SELECT * FROM users WHERE users.key="'. $key.'"');
+		$response = (count($response) < 1)?false:true;
+		return $response;
+	}
+
+	/*
+	*Get User By Token Key
+	*/
+	public function getUserByKey($key){
+		$response = $this->db->exec('SELECT * FROM users WHERE users.key="'. $key.'"');
+		return $response;
+	}
+
+	/*
+	*Check if user is admin
+	*/
+	public function isAdmin($key){
+		$req = $this->db->exec('SELECT user_group FROM users WHERE users.key="'. $key.'" AND users.user_group ="1"');
+		$response = (count($req) < 1)?false:true;
+		return $response;
+	}
+
+	/*
+	*Return all movies 
+	*/
+	public function getLikes($token){
+		$user = $this->getUserByKey($token);
+		$id = $user[0]['id'];
+		$req = $this->db->exec('SELECT movies.* FROM users_movies_likes as l, movies WHERE l.id_user="'. $id.'" AND l.id_movie=movies.id');
+		return $req;
+	}
+
+	public function getWishes($token){
+		$user = $this->getUserByKey($token);
+		$id = $user[0]['id'];
+		$req = $this->db->exec('SELECT movies.* FROM users_movies_wishes as w, movies WHERE w.id_user="'. $id.'" AND w.id_movie=movies.id');
+		return $req;
+	}
+
+	public function getViewed($token){
+		$user = $this->getUserByKey($token);
+		$id = $user[0]['id'];
+		$req = $this->db->exec('SELECT movies.* FROM users_movies_viewed as v, movies WHERE v.id_user="'. $id.'" AND v.id_movie=movies.id');
+		return $req;
+	}
+
+	public function getLikesFor($id){
+		$req = $this->db->exec('SELECT movies.* FROM users_movies_likes as l, movies WHERE l.id_user="'. $id.'" AND l.id_movie=movies.id');
+		return $req;
+	}
+
+	public function getWishesFor($id){
+		$req = $this->db->exec('SELECT movies.* FROM users_movies_wishes as w, movies WHERE w.id_user="'. $id.'" AND w.id_movie=movies.id');
+		return $req;
+	}
+
+	public function getViewedFor($id){
+		$req = $this->db->exec('SELECT movies.* FROM users_movies_viewed as v, movies WHERE v.id_user="'. $id.'" AND v.id_movie=movies.id');
+		return $req;
 	}
 }
